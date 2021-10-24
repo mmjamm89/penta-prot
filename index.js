e7072feef4e699f5ae4e1fb60f2b9ae7e5d41c00
Juan Martinez <78063884+mmjamm89@users.noreply.github.com>
2021-10-24 16:33:21 -0300

    third commit

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 // Animacion intro
-
 const introAnimation = () => {
 
   let tl = gsap.timeline({defaults: {ease: "power1.out"}});
 
-  tl.fromTo(".logo", {opacity: 0}, {opacity: 1, duration: 2})
-    .to(".text", { opacity: 0, duration: 1}, "-=0.5")
+  tl.fromTo(".logo", {opacity: 0}, {opacity: 1, duration: 2})    
     .to(".slider", { y: "-100%", duration: 1})
     .to(".intro-content", { y: "-100%", duration: 1}, "-=1")
 
 }
+
+const menuBtn = document.querySelector('.menu-btn');
+const navUl = document.querySelector('.nav-links')
+
+let menuOpen = false;
+
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen){
+    menuBtn.classList.add('open');
+    navUl.classList.toggle('show');
+    menuOpen = true;
+  }else{
+    menuBtn.classList.remove('open');
+    navUl.classList.toggle('show');
+    menuOpen = false;
+  }
+})
+
 
 introAnimation();
